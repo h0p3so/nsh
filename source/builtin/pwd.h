1e39f582d84f9fb218f30819a77862dba7aa19cd
h0p3so <juan.dpmz05@gmail.com>
2026-07-14 09:00:00 +0000

    bug solved

--- source/builtin/pwd.h
+++ source/builtin/pwd.h
@@ -4,16 +4,17 @@
 #include "shared/builtintypes.h"
 
 void builtin_pwd_set_origin (void);
-void builtin_pwd_cmd_run (const struct ParserTreeCmd*);
 void builtin_pwd_go_home (void);
-void builtin_pwd_set_path_to (const char*);
-void builtin_pwd_clean (void);
+void builtin_pwd_aux_add (const char*, const size_t);
 
+void builtin_pwd_cmd_run (const struct ParserTreeCmd*);
 static const struct BuiltInCmd GLOB_BUILTIN_PWD_CMD_META =
 {
 	.name = "pwd",
 	.help = "print working dir",
 	.run = builtin_pwd_cmd_run
 };
+
+void builtin_pwd_clean (void);
 
 #endif
