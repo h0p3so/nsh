58a3bd42b16f7f8a031582996df3dbfbcc8e0c37
h0p3so <juan.dpmz05@gmail.com>
2026-07-24 22:27:47 -0500

    CHECKPTR now belongs to err

--- source/err.h
+++ source/err.h
@@ -1,6 +1,11 @@
 #ifndef NHS_ERR_H
 #define NHS_ERR_H
 
+#define NSH_ERR_CHECKPTR(p, s, m) do { \
+	if (p) { break; }                     \
+	err_fatal(s, m);                      \
+} while (0)
+
 void err_fatal (const char*, const char*);
 
 #endif
