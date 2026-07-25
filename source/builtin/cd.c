833bc65d2304c534752956bbf389aaf49a6cf41c
h0p3so <juan.dpmz05@gmail.com>
2026-07-25 13:46:31 -0500

    using tilde to indicate home

--- source/builtin/cd.c
+++ source/builtin/cd.c
@@ -3,6 +3,10 @@
 #include "pwd.h"
 #include "cd.h"
 
+#include <string.h>
+
+#define _BUILTIN_CD_HOME_ALIAS '~'
+
 // this file only process the argument given to `cd` command, the
 // file that actually changes the directory is `pwd`
 
@@ -10,4 +14,16 @@ void builtin_cd_cmd_run (const struct ParserTreeCmd *treeCmd)
 {
 	if (stdv_size(treeCmd->commandRelated.argv) == NSH_BUILTIN_COMM_ARG_OFF)
 	{ builtin_pwd_go_home(); return; }
+
+	const char *arg = stdv_get(treeCmd->commandRelated.argv, NSH_BUILTIN_COMM_ARG_OFF);
+	const size_t arglen = strlen(arg);
+
+	if (*arg == _BUILTIN_CD_HOME_ALIAS && arglen == 1)
+	{ builtin_pwd_go_home(); }
+
+	if (*arg == _BUILTIN_CD_HOME_ALIAS && arglen == 2 && arg[1] == '/')
+	{ builtin_pwd_go_home(); }
+
+
+	// TODO: oldpwd
 }
