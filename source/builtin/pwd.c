8ff6e22430216fd752f6563ed79eeef3f06069c8
h0p3so <juan.dpmz05@gmail.com>
2026-07-29 22:04:03 -0500

    pwd working

--- source/builtin/pwd.c
+++ source/builtin/pwd.c
@@ -6,11 +6,22 @@
  */
 
 #include "pwd.h"
+#include "shared/pathinfo.h"
 
 #include <stdio.h>
+#include <unistd.h>
+
+static struct PathPath _pwd_cwd = {0};
 
 void builtin_pwd_cmd_run (const struct ParserTreeCmd *treeCmd)
 {
 	(void) treeCmd;
-	printf("pwd invoked\n");
+
+	static bool firstcall = true;
+	if (firstcall)
+	{
+		getcwd(_pwd_cwd.path, NSH_SHARED_PATHINFO_PATH_MAX); // TODO handle
+		firstcall = false;
+	}
+	printf("%s\n", _pwd_cwd.path);
 }
